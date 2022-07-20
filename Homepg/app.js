53f9c1e3612d6d00a72d2521c86c0f22be41163c
Gautam Singla <ggsingla777@gmail.com>
2022-07-21 00:03:45 +0530

    Two sections added, fonts fixed, parallex added

--- Homepg/app.js
+++ Homepg/app.js
@@ -1,114 +1,133 @@
-var visited = false;
-window.onscroll = function() {
-  var institute_name = document.getElementsByClassName("institute_name");
-  var full_navbar = document.getElementsByTagName("header");
-  var logo = document.getElementById("logo_250");
-  var topbar = document.getElementById("top_bar");
-  if(window.scrollY > 100) {
-    topbar.style.display = "none";
-  for(var i=0;i<institute_name.length;i++){
-    logo.style.width="90px";
-    logo.style.height="90px";
-    logo.classList.remove("top-8");
-    logo.classList.add("top-0");
-    if(institute_name[i].classList.contains("text-xl")){
-      institute_name[i].classList.remove("text-xl");
-      institute_name[i].classList.add("text-lg");
+var visited = false
+window.onscroll = function () {
+  var institute_name = document.getElementsByClassName('institute_name')
+  var full_navbar = document.getElementsByTagName('header')
+  var logo = document.getElementById('logo_250')
+  var topbar = document.getElementById('top_bar')
+  if (window.scrollY > 100) {
+    topbar.style.display = 'none'
+    for (var i = 0; i < institute_name.length; i++) {
+      logo.style.width = '90px'
+      logo.style.height = '90px'
+      logo.classList.remove('top-8')
+      logo.classList.add('top-0')
+      if (institute_name[i].classList.contains('text-xl')) {
+        institute_name[i].classList.remove('text-xl')
+        institute_name[i].classList.add('text-lg')
+      } else if (institute_name[i].classList.contains('text-lg')) {
+        institute_name[i].classList.remove('text-lg')
+        institute_name[i].classList.add('text-sm')
+      }
     }
-    else if(institute_name[i].classList.contains("text-lg")){
-    institute_name[i].classList.remove("text-lg");
-    institute_name[i].classList.add("text-sm");
+    if (window.scrollY > 1000 && !visited) {
+      animateValueIntiator()
     }
-  }
-  if(window.scrollY > 1000 && !visited){
-    animateValueIntiator();
-  }
-}
-else{
-  logo.style.width="120px";
-  logo.style.height="120px";
-  topbar.style.display = "flex";
-  logo.classList.remove("top-0");
-  logo.classList.add("top-8");
-  for(var i=0;i<institute_name.length;i++){
-    // console.log(institute_name[i].classList);
-    if(institute_name[i].classList.contains("text-lg")){
-      institute_name[i].classList.remove("text-lg");
-      institute_name[i].classList.add("text-xl");
-    }
-    else if(institute_name[i].classList.contains("text-sm")){
-    institute_name[i].classList.remove("text-sm");
-    institute_name[i].classList.add("text-lg");
+  } else {
+    logo.style.width = '120px'
+    logo.style.height = '120px'
+    topbar.style.display = 'flex'
+    logo.classList.remove('top-0')
+    logo.classList.add('top-8')
+    for (var i = 0; i < institute_name.length; i++) {
+      // console.log(institute_name[i].classList);
+      if (institute_name[i].classList.contains('text-lg')) {
+        institute_name[i].classList.remove('text-lg')
+        institute_name[i].classList.add('text-xl')
+      } else if (institute_name[i].classList.contains('text-sm')) {
+        institute_name[i].classList.remove('text-sm')
+        institute_name[i].classList.add('text-lg')
+      }
     }
   }
-  }
-}
- 
-function openDropdown() {
-  var dropdown = document.getElementById("dropdown");
-    if(dropdown.classList.contains("hidden")){
-      dropdown.classList.remove("hidden");
-    }
-    else{
-      dropdown.classList.add("hidden");
-    }
 }
 
-function showSearchPage(event){
-  if(event.target.id=="search_form"){
-    return;
+function openDropdown() {
+  var dropdown = document.getElementById('dropdown')
+  if (dropdown.classList.contains('hidden')) {
+    dropdown.classList.remove('hidden')
+  } else {
+    dropdown.classList.add('hidden')
   }
-  var search_page = document.getElementById("search_page");
-  if(search_page.classList.contains("hidden")){
-    search_page.classList.remove("hidden");
-    search_page.classList.add("grid");
+}
+
+function showSearchPage(event) {
+  if (event.target.id == 'search_form') {
+    return
   }
-  else{
-    search_page.classList.remove("grid");
-    search_page.classList.add("hidden");
+  var search_page = document.getElementById('search_page')
+  if (search_page.classList.contains('hidden')) {
+    search_page.classList.remove('hidden')
+    search_page.classList.add('grid')
+  } else {
+    search_page.classList.remove('grid')
+    search_page.classList.add('hidden')
   }
 }
 
 function isInViewPort(element) {
-  var bounding = element.getBoundingClientRect();
+  var bounding = element.getBoundingClientRect()
   if (
-      bounding.top >= 0 &&
-      bounding.left >= 0 &&
-      bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
-      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.right <=
+      (window.innerWidth || document.documentElement.clientWidth) &&
+    bounding.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight)
   ) {
-      console.log('In the viewport! :)');
-      return true;
+    console.log('In the viewport! :)')
+    return true
   } else {
-      console.log('Not in the viewport. :(');
-      return false;
+    console.log('Not in the viewport. :(')
+    return false
   }
 }
-function animateValue(obj, start, end, duration,index) {
-  if(isInViewPort(obj)){
-  let startTimestamp = null;
-  const step = (timestamp) => {
-    if (!startTimestamp) startTimestamp = timestamp;
-    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-    if(index==0 || index==1){
-      obj.innerHTML = Math.floor(progress * (end - start) + start) + "+";
+function animateValue(obj, start, end, duration, index) {
+  if (isInViewPort(obj)) {
+    let startTimestamp = null
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1)
+      if (index == 0 || index == 1) {
+        obj.innerHTML = Math.floor(progress * (end - start) + start) + '+'
+      } else {
+        obj.innerHTML = Math.floor(progress * (end - start) + start)
+      }
+      if (progress < 1) {
+        window.requestAnimationFrame(step)
+      }
     }
-    else{
-      obj.innerHTML = Math.floor(progress * (end - start) + start);
-    }
-    if (progress < 1) {
-      window.requestAnimationFrame(step);
-    }
-  };
-  window.requestAnimationFrame(step);
+    window.requestAnimationFrame(step)
   }
 }
 
-function animateValueIntiator(){
-  var rolling_nums = document.getElementById("rolling_nums").children;
-  visited=true;
-  for(var i=0;i<rolling_nums.length;i++){
-    obj=rolling_nums[i];
-    animateValue(obj,0,parseInt(obj.innerHTML),3000,i);
+function animateValueIntiator() {
+  var rolling_nums = document.getElementById('rolling_nums').children
+  visited = true
+  for (var i = 0; i < rolling_nums.length; i++) {
+    obj = rolling_nums[i]
+    animateValue(obj, 0, parseInt(obj.innerHTML), 3000, i)
   }
 }
+
+// Parallex Effect
+
+const numbers = document.getElementById('numbers')
+
+// window.addEventListener('scroll', () => {
+//   console.log('working ✅')
+//   let abvoe = numbers.offsetTop
+//   let offset = scrollY
+//   console.log(offset)
+//   numbers.style.backgroundPositionY = abvoe - offset * 0.7 + 'px'
+// })
+
+window.addEventListener('scroll', () => {
+  let offset = scrollY
+  const viewport = window.outerHeight
+  console.log('running')
+  if (offset >= numbers.offsetTop - viewport) {
+    console.log('touched')
+    numbers.style.backgroundPositionY =
+      -(numbers.offsetTop - offset) * 0.8 + 'px'
+  }
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
         sans: ['Barlow', 'ui-sans-serif', 'system-ui'],
         body: ['Barlow'],
       },
+      colors: {
+        'dark-purple-70': 'hsla(242, 83%, 7%, 0.7)',
+        'dark-purple': 'hsla(242, 83%, 7%, 1)',
+      },
+      backgroundImage: {
+        'number-bg': "url('./Homepg/img/bgnumbers.jpg')",
+      },
     },
   },
   plugins: [],
