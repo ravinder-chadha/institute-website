d3f29e5f1fabb861106bc279d928def457323a21
Gautam Singla <ggsingla777@gmail.com>
2022-09-28 23:58:41 +0530

    Admission section rebuilt

--- Homepg/app.js
+++ Homepg/app.js
@@ -194,6 +194,27 @@ const openCourse = (e) => {
   document.getElementById(e.dataset.course).classList.remove('hidden')
 }
 
+const notificationTabs = (e) => {
+  let tabcontent = document.getElementsByClassName('notice-content')
+  let tablinks = document.getElementsByClassName('notif-link')
+
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].classList.add('hidden')
+  }
+
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.add('hover:bg-blue-50', 'text-slate-400')
+    tablinks[i].classList.remove(
+      'bg-blue-100',
+      'text-accent',
+      'hover:bg-blue-200'
+    )
+  }
+  e.classList.remove('hover:bg-blue-50', 'text-slate-400')
+  e.classList.add('bg-blue-100', 'hover:bg-blue-200', 'text-accent')
+  document.getElementById(e.dataset.notif).classList.remove('hidden')
+}
+
 // ///////////////////////////
 // Image Slider Animation::
 
